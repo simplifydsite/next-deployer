a91e2604edf42568de8e4dc4ac08e48e4ce550b1
d4ndel1on <stefan@fancynetwork.net>
2025-05-04 10:12:53 +0200

    improve testing

---
+++ test/Integration.test.ts
@@ -0,0 +1,16 @@
+import { Template } from 'aws-cdk-lib/assertions'
+import { buildNextJsHosting } from '../src'
+
+describe('Integration', () => {
+  test('Stack', () => {
+    const { app } = buildNextJsHosting({
+      stackName: 'NextJsApp',
+      account: '0000000000',
+      domainName: 's0na.de',
+      cname: 'app',
+      staticAssetsBucketName: 'static.assets',
+    })
+
+    Template.fromJSON(app)
+  })
+})
