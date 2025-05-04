e299dd88f48a0e2225b623779967addba21e3809
d4ndel1on <stefan@fancynetwork.net>
2025-05-04 13:50:45 +0200

    improve code

--- scripts/deploy_infrastructure.sh
+++ scripts/deploy_infrastructure.sh
@@ -23,4 +23,4 @@ export CNAME
 export DOMAIN_NAME
 export AWS_REGION
 
-npx cdk deploy --app "npx tsx node_modules/@d4ndel1on/next-deployer/src/index.ts" --all --profile "${2}"
+npx cdk@latest deploy --app "npx tsx node_modules/@d4ndel1on/next-deployer/src/index.ts" --all --profile "${2}"
