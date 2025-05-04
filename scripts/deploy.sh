98aeb2b611cba74b9b68b2e24a8053a0d54ae490
d4ndel1on <stefan@fancynetwork.net>
2025-05-04 17:38:58 +0200

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,11 +10,11 @@ fi
 
 if [ -z "${2}" ];
 then
+  echo "Using credentials from environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY"
+else
   echo "Using AWS_PROFILE ${2}"
   export AWS_PROFILE="${2}"
-  export PROFILE_STRING=--profile "${2}"
-else
-  echo "Using credentials from environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY"
+  export PROFILE_STRING="--profile"
 fi
 
 echo "Using env file .env.${1}"
@@ -30,8 +30,8 @@ echo "Using s3 bucket ${S3_BUCKET}"
 echo "Using stack name ${STACK_NAME}"
 npm run build
 echo "sync"
-aws s3 sync --delete out/ "s3://${S3_BUCKET}/" "${PROFILE_STRING}"
+aws s3 sync "${PROFILE_STRING}" "${AWS_PROFILE}" --delete out/ "s3://${S3_BUCKET}/"
 aws cloudfront create-invalidation \
-		--distribution-id "$(aws cloudfront list-distributions "${PROFILE_STRING}" | jq --arg stack_name "${STACK_NAME}" -r '.DistributionList.Items[] | select(.Comment==$stack_name) | .Id')" \
-		"${PROFILE_STRING}" \
+		--distribution-id "$(aws cloudfront list-distributions "${PROFILE_STRING}" "${AWS_PROFILE}" | jq --arg stack_name "${STACK_NAME}" -r '.DistributionList.Items[] | select(.Comment==$stack_name) | .Id')" \
+		"${PROFILE_STRING}" "${AWS_PROFILE}" \
 		--paths "/*"
