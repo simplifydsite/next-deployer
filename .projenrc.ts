9d0acf836d066df95efc5bfbf384820fc99d162c
d4ndel1on <stefan@fancynetwork.net>
2025-05-18 11:24:16 +0200

    publish to npmjs

--- .projenrc.ts
+++ .projenrc.ts
@@ -5,7 +5,7 @@ import { NodePackageManager } from 'projen/lib/javascript'
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'd4ndel1on',
   authorAddress: 'github.overfed135@passmail.net',
-  cdkVersion: '2.194.0',
+  cdkVersion: '2.196.0',
   defaultReleaseBranch: 'main',
   name: '@d4ndel1on/next-deployer',
   projenrcTs: true,
@@ -16,7 +16,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
   },
   packageManager: NodePackageManager.NPM,
   minNodeVersion: '22.15.0',
-  npmRegistryUrl: 'https://npm.pkg.github.com',
   bundledDeps: [
     'humps',
     'fs-extra',
@@ -64,27 +63,6 @@ project.tryFindObjectFile('.github/workflows/build.yml')!
   .addOverride('jobs.build.steps.8.with', {
     'retention-days': 5,
     'include-hidden-files': 'true',
-  })
-
-project.tryFindObjectFile('.github/workflows/release.yml')!
-  .addOverride('jobs.release.env', {
-    CI: 'true',
-    NPM_AUTH_TOKEN: '${{ secrets.PROJEN_GITHUB_TOKEN }}',
-  })
-
-project.tryFindObjectFile('.github/workflows/release.yml')!
-  .addOverride('jobs.release_npm.env', {
-    NPM_AUTH_TOKEN: '${{ secrets.PROJEN_GITHUB_TOKEN }}',
-  })
-
-project.tryFindObjectFile('.github/workflows/build.yml')!
-  .addOverride('jobs.build.env', {
-    NPM_AUTH_TOKEN: '${{secrets.PROJEN_GITHUB_TOKEN}}',
-  })
-
-project.tryFindObjectFile('.github/workflows/build.yml')!
-  .addOverride('jobs.package-js.env', {
-    NPM_AUTH_TOKEN: '${{secrets.PROJEN_GITHUB_TOKEN}}',
   })
 
 project.addTask('pack', {
